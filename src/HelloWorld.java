258dc7f33f47e6ca9cd5821c98e6bc3160924442
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-07-29 23:11:07 -0500

    Created new java file for warmups and codeup exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("Another line of code");
+        long number = 333;
+        String niceWords = "I love being alive";
+        boolean isTrue = true;
+    }
+}
