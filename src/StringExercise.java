d2fa188380d9079950ca9f553719aa65b7d570dc
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-02 17:24:55 -0500

    Added notes and some bonus exercises to complete during down time.

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,16 +1,34 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String stringStatement =  "We don't need no education \n" + "We don't need no thought control";
-        System.out.println(stringStatement);
+//        STRING EXERCISE 1: PRINT!
+//        String stringStatement =  "We don't need no education \n" + "We don't need no thought control";
+//        System.out.println(stringStatement);
+//
+//        String quotationStatement = "Check \"this\" out! \"s inside of \"s!";
+//        System.out.println(quotationStatement);
+//
+//        String windowsString = "In windows, the main drive is usually C:\\";
+//        System.out.println(windowsString);
+//
+//        String backslashString = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+//        System.out.println(backslashString);
 
-        String quotationStatement = "Check \"this\" out! \"s inside of \"s!";
-        System.out.println(quotationStatement);
 
-        String windowsString = "In windows, the main drive is usually C:\\";
-        System.out.println(windowsString);
+// BONUS EXERCISES TO COMPLETE DURING DOWN TIME!
+//        DATE CONVERTER
+//        Change date from MM/DD/YYYY format
+//        To Month DD, YYYY
 
-        String backslashString = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
-        System.out.println(backslashString);
+//        SENTENCE ANALYZER
+//        Create sentence analyzer program that tells the user how many vowels and consonants were
+//used.
+
+//        COMMAND LINE CALCULATOR
+//        The program should welcome the user, prompt them for a calculation, and give the result. The
+//        user should be prompted after each result is outputted if they want to enter another calculation.
+//        When they say no, give them an exit message. Allow the user first to specify how many
+//        operands they will enter. Do not worry about input validation (assume user enters correct data
+//        type)
 
     }
 }
