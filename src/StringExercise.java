9416ee0b205c7e78b182fcd9eb69b1a7e0b75185
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-02 14:48:37 -0500

    Created and completed exercise 1 under codeup Java strings exercises.

---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String stringStatement =  "We don't need no education \nWe don't need no thought control";
+        System.out.println(stringStatement);
+
+        String quotationStatement = "Check \"this\" out! \"s inside of \"s!";
+        System.out.println(quotationStatement);
+
+        String windowsString = "In windows, the main drive is usually C:\\";
+        System.out.println(windowsString);
+
+        String backslashString = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(backslashString);
+    }
+}
+
+
