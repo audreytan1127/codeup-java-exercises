b438062daf40cac90876c55306259cf0df1f7b05
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-07 10:06:32 -0500

    Completed class walk thru for input and input test

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -9,7 +9,7 @@ public class InputTest {
 //        String usersName = userInput.getString();
 //        System.out.printf("The users name is %s", usersName);
 
-//        //TEST BOOLEAN METHOD
+////        //TEST BOOLEAN METHOD
 //        System.out.println("Do you want a cookie?[yes/no]");
 //        Input userInput = new Input();
 //        boolean userConfirm = userInput.yesNo();
@@ -17,6 +17,7 @@ public class InputTest {
 
 //        //TEST GETINT(min, max) METHOD
 //        Input userInput = new Input();
+//        System.out.println("Please enter an integer between 1 and 10.");
 //        int userInputInt = userInput.getInt(1,10);
 //        System.out.printf("The number you chose is %d", userInputInt);
 
@@ -32,9 +33,12 @@ public class InputTest {
 //        System.out.println(userInputNum);
 
 //        //TEST GETDOUBLE METHOD
-//        Input userInput = new Input();
+        Input userInput = new Input();
 //        System.out.println("Please enter a number.");
 //        double userInputNum = userInput.getDouble();
 //        System.out.printf("You chose the number %.2f.%n", userInputNum);
+
+        //TEST GETSTRING WITH PROMPT
+        System.out.println(userInput.getString("Enter your name."));
     }
 }
