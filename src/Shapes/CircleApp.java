ba5159f56954857ff2081a8703d350a3159c04d0
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-07 10:44:50 -0500

    added bonus do while loop to ask user to get new areas and circumferences.

--- src/Shapes/CircleApp.java
+++ src/Shapes/CircleApp.java
@@ -7,28 +7,29 @@ import util.Input;
 public class CircleApp {
 
 //    // Call the Input class put into variable so it can be called easily.
-    static Input userInput = new Input();
 
 //    // Just to test, put all of the calls to Circle class in psvm
 //    //Move the variables out of main and leave souts when done testing.
     public static void main(String[] args) {
+     Input userInput = new Input();
 //        // Ask user for the radius of the circle
-        System.out.println("Please enter the radius of your circle.");
+       do {
+            System.out.println("Please enter the radius of your circle.");
 //        // create variable to store the users input for the radius
 //        // use Input class and get Double value
-        double userRadius = userInput.getDouble();
+            double userRadius = userInput.getDouble();
 //        // Create new circle by calling Circle class
 //        // give the new circle the name of firstCircle
 //        // set it equal to a new Circle with the parameter of the users input radius value.
-        Circle firstCircle = new Circle(userRadius);
+            Circle firstCircle = new Circle(userRadius);
 //        // set area variable of firstCircle equal to the firstCircle's area
 //        // get area by calling getArea() method from Circle class (file)
-        double firstCircleArea = firstCircle.getArea();
+            double firstCircleArea = firstCircle.getArea();
 //        // set circumference variable of firstCircle equal to the firstCircle's circumference
 //        // get circumference by calling getCircumference() method from Circle class (file)
-        double firstCircleCircumference = firstCircle.getCircumference();
+            double firstCircleCircumference = firstCircle.getCircumference();
 //        // Sout the area and circle with restricted decimal places
-        System.out.printf("Your circle's area = %.2f.%n Your circle's circumference = %.2f.%n", firstCircleArea, firstCircleCircumference);
-
+            System.out.printf("Your circle's area = %.2f.%n Your circle's circumference = %.2f.%n", firstCircleArea, firstCircleCircumference);
+        } while(userInput.yesNo("Would you like to make another circle? [y/n]"));
     }
 }
