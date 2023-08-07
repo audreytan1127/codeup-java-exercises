b98688fb8d5ef52f149ebe190d71cdf95b385188
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-07 15:10:10 -0500

    Created and completed ArraysExercise with class walk through notes

---
+++ src/ArraysExercise.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class ArraysExercise {
+    private static void arrayBasics(){
+                int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+    Person[] people = {
+            new Person("Bob"),
+            new Person("Patty"),
+            new Person("Eugene")
+    };
+
+    for(Person person : people){
+        System.out.println(person.getName());
+    }
+//  // EXERCISE 1 PRINT NAMES OF EACH PERSON IN NEW PEOPLE ARRAY WITH ADDED PERSON
+        Person[] people2 = addPerson(people, new Person("Julia"));
+        for (int i = 0; i < people2.length; i++) {
+            System.out.println(people2[i].getName());
+        }
+    }
+
+// // Add person to people array
+    private static Person[] addPerson(Person[] people, Person addPerson){
+        Person[] newPeople = Arrays.copyOf(people, people.length + 1);
+        newPeople[newPeople.length -1] = addPerson;
+        people = newPeople;
+        return newPeople;
+    }
+
+    public static void main(String[] args) {
+        arrayBasics();
+////        // Prints the entire array within [].
+//        System.out.println(Arrays.toString(numbers));
+
+//        //EXERCISE 1 PRINT NAMES EACH PERSON IN PEOPLE ARRAY
+//        Person[] people;
+//        people = new Person[2];
+//        people = new Person[]{new Person("Audrey"), new Person("Drake"), new Person("Amber")};
+//
+//        for(int i=0; i< people.length; i++){
+//            System.out.println(people[i].getName());
+//        }
+
+
+
+    }
+}
