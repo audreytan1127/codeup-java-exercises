390ff88d2aea25df6fdab4201fe6f8deac8822ee
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-08 09:09:09 -0500

    added protected onto variables and this keyword to length and width

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -3,7 +3,7 @@ package Shapes;
 public class Rectangle {
 
     protected double length;
-    double width;
+    protected double width;
 
     public Rectangle(double length, double width) {
         this.length = length;
@@ -12,12 +12,12 @@ public class Rectangle {
 
 
     public double getArea(){
-        double area = length * width;
+        double area = this.length * this.width;
         return area;
     }
 
     public double getPerimeter(){
-        double perimeter = (2*length) + (2*width);
+        double perimeter = (2*this.length) + (2*this.width);
         return perimeter;
     }
 
