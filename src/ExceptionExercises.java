08a76780dccf23fd531af8a99864b8b082a6b667
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-09 16:56:29 -0500

    Created new ExceptionExercises class and finished required exercises. Not the bonus..

---
+++ src/ExceptionExercises.java
@@ -0,0 +1,52 @@
+public class ExceptionExercises {
+    /* ================================= EXERCISES
+
+Create a class called ExceptionExercises and complete the following steps.
+
+STEP 1
+
+Create a static divideNums method that takes in two integer arguments. If the second argument is 0, the method should throw an IllegalArgumentException with the message "Cannot divide be 0!" If the second argument is not 0, return the result of dividing the first input integer by the second input integer.
+
+STEP 2
+
+Add a main method and try calling the divideNums method with various arguments, including 0 for the second argument. Is IllegalArgumentException a checked or unchecked exception?
+
+STEP 3
+
+Create a public static void method called delayedHello that takes in an integer argument for the number of milliseconds of delay before "Hello World" is printed to the console. Use Thread.sleep() in the method to accomplish this. Does Thread.sleep() throw a checked or unchecked exception?
+
+STEP 4
+
+Test the delayedHello method with various delay lengths.
+
+
+-- BONUS
+
+Use Thread.sleep() to create a program that will take in a string of input and display it in the console as if someone where typing each character. Consider that the delay between one character being printed and the next may randomly vary between a certain range of delays.*/
+
+    public static void divideNums(int int1, int int2){
+        try{
+            int divisionOfInt = int1 / int2;
+            System.out.println(divisionOfInt);
+        } catch(ArithmeticException e){
+            if(int2 == 0){
+                System.out.println("Cannot divide by 0!");
+            }
+        }
+    }
+
+    public static void delayedHello(int milliseconds){
+            try {
+                Thread.sleep(milliseconds);
+                System.out.println("Hello World!");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+    }
+
+    public static void main(String[] args) {
+        divideNums(5,0);
+        divideNums(4,2);
+        delayedHello(1000);
+    }
+}
